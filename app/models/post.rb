7476450f81675e75b7108e1b7121f33eb91c572b
Andy Ly <andyly25@yahoo.com>
2016-05-02 16:55:42 -0700

    Since there needs to be a default image, I added in an iceblock I made.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :title, use: :slugged
-	has_attached_file :image, styles:{large:"600x600>", medium:"300x300>",thumb:"150x150#"}
+	# I used that for default url since I wasn't able to do it the normal way
+	has_attached_file :image, styles:{large:"600x600>", medium:"300x300>",thumb:"150x150#"}, 
+						default_url: lambda{|image| ActionController::Base.helpers.asset_path('iceblock.png')}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ActiveRecord::Base
 	extend FriendlyId
 	friendly_id :title, use: :slugged
-	has_attached_file :image, styles:{large:"600x600>", medium:"300x300>",thumb:"150x150#"}
+	has_attached_file :image, styles:{large:"600x600>", medium:"300x300>",thumb:"150x150#"}, 
+	default_url: lambda{|image| ActionController::Base.helpers.asset_path('iceblock.png')}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
